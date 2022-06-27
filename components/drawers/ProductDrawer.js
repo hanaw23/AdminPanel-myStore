ecb1ba87270b6878539c26888fec1370cad472af
Hana Widyatari <hanawidyatari23@gmail.com>
2022-06-27 00:53:57 +0000

    Hanaw develop - update ui

--- components/drawers/ProductDrawer.js
+++ components/drawers/ProductDrawer.js
@@ -15,16 +15,16 @@ function ProductDrawer(props) {
 
   return (
     <main
-      className={` bg-gray-800 fixed  h-full overflow-hidden z-10 bg-opacity-25 inset-0 transform ease-in-out ${
-        props.isOpen ? " transition-opacity opacity-100 duration-500 translate-x-0 " : "transition-all delay-500 opacity-0 translate-x-full  "
+      className={` bg-gray-100 fixed  h-full overflow-hidden z-10 bg-opacity-25 inset-0 transform ease-in-out ${
+        props.isOpen ? " transition-opacity opacity-100 duration-500 translate-x-0 bg-gray-700 " : "transition-all delay-500 opacity-0 translate-x-full  "
       }`}
     >
       <section
-        className={`bg-gray-300 w-screen max-w-lg right-0 absolute h-full shadow-xl delay-400 duration-500 ease-in-out transition-all transform overflow-y-auto overflow-x-hidden text-gray-700 ${
+        className={`bg-gray-100 w-screen max-w-lg right-0 absolute h-full shadow-xl delay-400 duration-500 ease-in-out transition-all transform overflow-y-auto overflow-x-hidden text-gray-700 ${
           props.isOpen ? " translate-x-0 " : " translate-x-full "
         }`}
       >
-        <article className="relative w-screen max-w-lg p-8 flex flex-col h-full bg-gray-300">
+        <article className="relative w-screen max-w-lg p-8 flex flex-col h-full bg-gray-100">
           <div className="flex justify-between">
             <header className="font-bold text-lg text-left flex">{props.title} Product</header>
             {props.showCloseButton ? (
