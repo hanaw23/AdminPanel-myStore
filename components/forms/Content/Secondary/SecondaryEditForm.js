ce1c0c1ff764e8cc6aba65901ab0f2be9266c5fd
Hana Widyatari <hanawidyatari23@gmail.com>
2022-06-29 14:10:29 +0700

    fixing success and failed modal msg cont secondary

--- components/forms/Content/Secondary/SecondaryEditForm.js
+++ components/forms/Content/Secondary/SecondaryEditForm.js
@@ -43,13 +43,7 @@ function SecondaryEditForm(props) {
           <label htmlFor="name" className="text-indigo-700">
             Name
           </label>
-          <input
-            id="name"
-            className="text-gray-700 px-3 border border-gray-300 rounded w-[320px] h-10 mt-2  text-gray-700 focus:outline-blue-500"
-            placeholder="Input Secondary Name"
-            onChange={(event) => setName(event.target.value)}
-            value={name}
-          />
+          <input id="name" className="text-gray-700 px-3 border border-gray-300 rounded w-[320px] h-10 mt-2 focus:outline-blue-500" placeholder="Input Secondary Name" onChange={(event) => setName(event.target.value)} value={name} />
         </div>
         <div className="mt-4 flex flex-col">
           <label htmlFor="description" className="text-indigo-700">
@@ -57,7 +51,7 @@ function SecondaryEditForm(props) {
           </label>
           <textarea
             id="description"
-            className="text-gray-700 px-3 border border-gray-300 rounded w-[500px] h-[100px] mt-2  text-gray-700 focus:outline-blue-500 pt-8"
+            className="text-gray-700 px-3 border border-gray-300 rounded w-[500px] h-[100px] mt-2 focus:outline-blue-500 pt-8"
             placeholder="Type Secondary Description"
             onChange={(event) => setDescription(event.target.value)}
             value={description}
@@ -75,8 +69,8 @@ function SecondaryEditForm(props) {
           </div>
         </div>
       </div>
-      {success.length !== 0 && <SuccessMessage message={success} actionTitle="Edit Carousel" />}
-      {failed.length !== 0 && <ErrorMessage message={failed} actionTitle="Add Product" />}
+      {success.length !== 0 && <SuccessMessage message={success} actionTitle="Edit Secondary" />}
+      {failed.length !== 0 && <ErrorMessage message={failed} actionTitle="Edit Secondary" />}
     </>
   );
 }
