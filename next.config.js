bc956aba513443e7e372d9b6034ba4752271c7bb
Hana Widyatari <hanawidyatari23@gmail.com>
2022-07-01 15:57:17 +0700

    fix bug redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const redirectConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+      },
+    ];
+  },
+};
+
+module.exports = { nextConfig, redirectConfig };
