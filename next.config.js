9c4a472328c3218fec3945a5d33ab95451bff34b
Hana Widyatari <hanawidyatari23@gmail.com>
2022-07-01 16:12:23 +0700

    redirect bug fix

--- next.config.js
+++ next.config.js
@@ -4,14 +4,16 @@ const nextConfig = {
 };
 
 const redirectConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/",
-        destination: "/login",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/login",
+  //     },
+  //   ];
+  // },
+
+  basePath: "/login",
 };
 
 module.exports = { nextConfig, redirectConfig };
