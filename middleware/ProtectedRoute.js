1378aa58ba345f6f254b6e1e52aeae3bd9fbe3ec
Hana Widyatari <hanawidyatari23@gmail.com>
2022-07-04 13:09:35 +0700

    try deployment new

--- middleware/ProtectedRoute.js
+++ middleware/ProtectedRoute.js
@@ -1,5 +1,5 @@
 import Router from "next/router";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import { GetToken, GetUser, HasToken } from "../utility";
 
 export const ProtectedRoute = () => {
