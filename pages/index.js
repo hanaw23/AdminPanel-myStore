f302cea3486cf58aeb8de11eb9e017ea569f7ee5
Hana Widyatari <hanawidyatari23@gmail.com>
2022-07-04 12:42:30 +0700

    new

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 const Home = () => {
   const router = useRouter();
   useEffect(() => {
-    router.push("/productManagement");
+    router.push("/contentManagement");
   }, []);
 };
 
