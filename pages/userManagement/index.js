14f53b32c1baeade278dc4d815251e4b33a11ff6
Hana Widyatari <hanawidyatari23@gmail.com>
2022-07-06 14:12:11 +0700

    fix eslint user

--- pages/userManagement/index.js
+++ pages/userManagement/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { useEffect } from "react";
 import Layout from "../../components/Layout";
 import LoadingSkeleton from "../../components/Loading/LoadingSkeleton";
@@ -16,9 +17,7 @@ export default function userManagement() {
       <div className="z-0">
         <div className="flex justify-between h-14"></div>
         <div>
-          <h1 className=" font-semibold text-[30px] text-gray-600 ml-20 -mt-[40px]">
-            User Management
-          </h1>
+          <h1 className=" font-semibold text-[30px] text-gray-600 ml-20 -mt-[40px]">User Management</h1>
         </div>
         <div className=" -ml-[70px] mt-20 mx-center">
           <LoadingSkeleton />
