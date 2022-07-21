4de56fd2b8668d9b2362b2fabbb370c18131bbd6
Hana Widyatari <hanawidyatari23@gmail.com>
2022-07-21 17:48:11 +0700

    fixing ui for user management

--- common/TableHeader/UserTableHeader.js
+++ common/TableHeader/UserTableHeader.js
@@ -1,43 +1,48 @@
-import DeleteButton from '../../components/Buttons/DeleteButton';
-import DetailButton from '../../components/buttons/DetailButton';
+import DeleteButton from "../../components/Buttons/DeleteButton";
+import DetailButton from "../../components/buttons/DetailButton";
+import style from "./User.module.css";
 
 class UserTableHeader {
   static columns = [
     {
-      name: 'User Id',
+      name: "User Id",
       selector: (row) => row.user_id,
       sortable: true,
     },
     {
-      name: 'Username',
+      name: "Username",
       selector: (row) => row.username,
       sortable: true,
     },
     {
-      name: 'Email',
+      name: "Email",
       selector: (row) => row.email,
       sortable: true,
     },
     {
-      name: 'Gender',
+      name: "Gender",
       selector: (row) => row.gender,
-      cell: (row) => (row.gender === 'male' ? (
-        <div className="border border-blue-800 rounded-[10px] w-[80px]" key={row.gender}>
-          <h3 className="text-blue-500 text-xm px-1 py-1 text-center ">{row.gender}</h3>
-        </div>
-      ) : (
-        <div className="border border-rose-800 rounded-[10px] w-[80px]" key={row.gender}>
-          <h3 className="text-rose-500 text-xm px-1 py-1 text-center ">{row.gender}</h3>
-        </div>
-      )),
+      cell: (row) => (
+        <>
+          {row.gender === "male" ? (
+            <div className={`${style.border}  `} key={row.gender}>
+              <p className={` ${style.male} `}>{row.gender}</p>
+            </div>
+          ) : (
+            <div className={`${style.border} `} key={row.gender}>
+              <p className={` ${style.female}`}>{row.gender}</p>
+            </div>
+          )}
+        </>
+      ),
     },
     {
-      name: 'Address',
+      name: "Address",
       selector: (row) => row.address,
       sortable: true,
     },
     {
-      name: 'Action',
+      name: "Action",
       cell: (row) => (
         <div className="flex gap-6">
           <DetailButton userId={row.user_id} username={row.username} role={row.role} email={row.email} gender={row.gender} address={row.address} />
--- components/tables/UserTable.js
+++ components/tables/UserTable.js
@@ -1,18 +1,18 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from 'react';
-import DataTable from 'react-data-table-component';
-import { useDispatch } from 'react-redux';
+import { useEffect, useState } from "react";
+import DataTable from "react-data-table-component";
+import { useDispatch } from "react-redux";
 
-import UserTableHeader from '../../common/TableHeader/UserTableHeader';
-import LoadingSpinner from '../Loading/LoadingSpinner';
-import SearchFilter from '../filters/SearchFilter';
+import UserTableHeader from "../../common/TableHeader/UserTableHeader";
+import LoadingSpinner from "../Loading/LoadingSpinner";
+import SearchFilter from "../filters/SearchFilter";
 
-import { getUserAxios } from '../../store/Action/UserManagement';
+import { getUserAxios } from "../../store/Action/UserManagement";
 
 function UserTable() {
   const [pending, setPending] = useState(true);
   const [users, setUsers] = useState([]);
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
 
   const dispatch = useDispatch();
 
@@ -28,7 +28,7 @@ function UserTable() {
   }, []);
 
   const handleData = (rowData) => {
-    if (query === '') {
+    if (query === "") {
       return rowData;
     }
     const filteredItems = rowData.filter((item) => JSON.stringify(item).toLowerCase().indexOf(query.toLowerCase()) !== -1);
@@ -36,12 +36,12 @@ function UserTable() {
   };
 
   return (
-    <>
+    <div className="lg:text-base text-sm">
       <SearchFilter filterValue={query} filterChange={(e) => setQuery(e.target.value)} />
       <div className="block min-w-full shadow rounded-lg overflow-x-auto whitespace-nowrap -ml-4 py-3 w-80 mt-6 xl:text-base 2xl:text-base text-sm">
         <DataTable columns={UserTableHeader.columns} data={handleData(users.user)} pagination progressPending={pending} progressComponent={<LoadingSpinner />} highlightOnHovers />
       </div>
-    </>
+    </div>
   );
 }
 
